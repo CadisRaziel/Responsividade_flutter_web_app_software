b48fd4c8217422ab2dfad319ee694607eb1856ce
CadisRaziel <vitorcsharp@hotmail.com>
2021-08-18 11:05:42 -0300

    conceitos avancados

---
+++ lib/03_conceitos_avancados/constraints.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Constrainti extends StatefulWidget {
+  const Constrainti({ Key? key }) : super(key: key);
+
+  @override
+  _ConstraintiState createState() => _ConstraintiState();
+}
+
+class _ConstraintiState extends State<Constrainti> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
